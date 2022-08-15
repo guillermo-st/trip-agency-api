dd2a10f3dbcedc34371bb92316e3c3af2e3afb0d
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2022-08-15 20:56:14 -0300

    DB implementation for DriverRepo Interface

--- go.mod
+++ go.mod
@@ -3,3 +3,5 @@ module github.com/guillermo-st/trip-agency-api
 go 1.18
 
 require golang.org/x/crypto v0.0.0-20220722155217-630584e8d5aa
+
+require github.com/jmoiron/sqlx v1.3.5 // indirect
---
+++ repositories/db_driver_repo.go
@@ -0,0 +1,71 @@
+package repositories
+
+import (
+	"errors"
+
+	"github.com/guillermo-st/trip-agency-api/models"
+	"github.com/jmoiron/sqlx"
+)
+
+type DBDriverRepository struct {
+	db *sqlx.DB
+}
+
+func (repo *DBDriverRepository) GetDrivers(pageSize uint, pageNum uint) ([]models.User, error) {
+	var drivers []models.User
+
+	query := `SELECT user_id, first_name, last_name, email, password, role_id 
+				FROM users 
+				ORDER BY user_id ASC
+				LIMIT ? OFFSET ?;`
+
+	offset := pageNum * pageSize
+
+	err := repo.db.Select(&drivers, query, pageSize, offset)
+	if err != nil {
+		return nil, err
+	}
+	return drivers, nil
+}
+
+func (repo *DBDriverRepository) GetDriversByStatus(pageSize uint, pageNum uint, isOnTrip bool) ([]models.User, error) {
+
+	var drivers []models.User
+
+	queryFinishedAt := "IS NULL"
+	query := `SELECT u.user_id, u.first_name, u.last_name, u.email, u.password, u.role_id 
+				FROM users u INNER JOIN trips t ON t.user_id = u.user_id
+				WHERE t.finished_at ? AND u.role_id = 2
+				ORDER BY user_id ASC
+				LIMIT ? OFFSET ?;`
+
+	if !isOnTrip {
+		noTripsQuery := `SELECT u.user_id, u.first_name, u.last_name, u.email, u.password, u.role_id 
+					FROM users u
+					WHERE NOT EXISTS
+					(SELECT t.trip_id FROM trips t WHERE t.user_id = u.user_id)
+					AND u.role_id = 2
+					UNION `
+
+		query = noTripsQuery + query
+		queryFinishedAt = "IS NOT NULL"
+	}
+
+	offset := pageNum * pageSize
+	err := repo.db.Select(&drivers, query, queryFinishedAt, pageSize, offset)
+	return drivers, err
+}
+
+func (repo *DBDriverRepository) AddDriver(driver models.User) error {
+	var err error = nil
+	defer func() {
+		if r := recover(); r != nil {
+			err = errors.New("Panicked while starting DB transaction")
+		}
+	}()
+
+	tx := repo.db.MustBegin()
+	tx.MustExec("INSERT INTO users (first_name, last_name, email, password, role_id) VALUES($1, $2, $3, $4, $5);", driver.FirstName, driver.LastName, driver.Email, driver.Password, 2)
+	err = tx.Commit()
+	return err
+}
