9f44b937591d606989af5482af0332b68c3bb750
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2022-08-15 13:28:43 -0300

    Define interfaces for the repository layer

---
+++ repositories/repository.go
@@ -0,0 +1,18 @@
+package repositories
+
+import "github.com/guillermo-st/trip-agency-api/models"
+
+type DriverRepository interface {
+	GetDrivers(pageSize uint, pageNum uint) ([]models.User, error)
+	GetDriversByStatus(pageSize uint, pageNum uint, isOnTrip bool) ([]models.User, error)
+	AddDriver(driver models.User) error
+}
+
+type TripRepository interface {
+	StartNewTripForDriver(driver models.User) error
+	FinishTripForDriver(driver models.User) error
+}
+
+type UserRepository interface {
+	GetUserByEmail(email string) (*models.User, error)
+}
