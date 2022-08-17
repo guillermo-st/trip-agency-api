241fd47de9ce33a5d739a0afc39b1a79dd403449
Guillermo Stancanelli <guillermo_stancanelli@outlook.com>
2022-08-17 10:06:05 -0300

    Make MockRepo fields accesible for testing

--- repositories/mock_repo.go
+++ repositories/mock_repo.go
@@ -7,8 +7,8 @@ import (
 )
 
 type MockRepository struct {
-	users            map[string]models.User
-	areDriversOnTrip map[string]bool
+	Users            map[string]models.User
+	AreDriversOnTrip map[string]bool
 }
 
 func NewMockRepository(initialUsers ...models.User) MockRepository {
@@ -24,7 +24,7 @@ func NewMockRepository(initialUsers ...models.User) MockRepository {
 }
 
 func (repo *MockRepository) GetUserByEmail(email string) (*models.User, error) {
-	user, present := repo.users[email]
+	user, present := repo.Users[email]
 	if !present {
 		return nil, errors.New("User not found.")
 	}
@@ -32,49 +32,49 @@ func (repo *MockRepository) GetUserByEmail(email string) (*models.User, error) {
 }
 
 func (repo *MockRepository) StartNewTripForDriver(driver models.User) error {
-	_, present := repo.areDriversOnTrip[driver.Email]
+	_, present := repo.AreDriversOnTrip[driver.Email]
 	if !present {
 		return errors.New("Can't start trip for unexisting driver")
 	}
 
-	repo.areDriversOnTrip[driver.Email] = true
+	repo.AreDriversOnTrip[driver.Email] = true
 	return nil
 }
 
 func (repo *MockRepository) FinishTripForDriver(driver models.User) error {
-	_, present := repo.areDriversOnTrip[driver.Email]
+	_, present := repo.AreDriversOnTrip[driver.Email]
 	if !present {
 		return errors.New("Can't finish trip for unexisting driver")
 	}
 
-	repo.areDriversOnTrip[driver.Email] = false
+	repo.AreDriversOnTrip[driver.Email] = false
 	return nil
 }
 
 func (repo *MockRepository) AddDriver(driver models.User) error {
-	_, present := repo.users[driver.Email]
+	_, present := repo.Users[driver.Email]
 	if present {
 		return errors.New("Driver already exists!")
 	}
 
-	repo.users[driver.Email] = driver
-	repo.areDriversOnTrip[driver.Email] = false
+	repo.Users[driver.Email] = driver
+	repo.AreDriversOnTrip[driver.Email] = false
 	return nil
 }
 
 func (repo *MockRepository) GetDrivers(pageSize uint, pageNum uint) ([]models.User, error) {
-	users := make([]models.User, 0, len(repo.users))
-	for _, user := range repo.users {
+	users := make([]models.User, 0, len(repo.Users))
+	for _, user := range repo.Users {
 		users = append(users, user)
 	}
 	return users, nil
 }
 
 func (repo *MockRepository) GetDriversByStatus(pageSize uint, pageNum uint, isOnTrip bool) ([]models.User, error) {
-	users := make([]models.User, 0, len(repo.users))
-	for userEmail, userOnTrip := range repo.areDriversOnTrip {
+	users := make([]models.User, 0, len(repo.Users))
+	for userEmail, userOnTrip := range repo.AreDriversOnTrip {
 		if userOnTrip == isOnTrip {
-			users = append(users, repo.users[userEmail])
+			users = append(users, repo.Users[userEmail])
 		}
 	}
 	return users, nil
